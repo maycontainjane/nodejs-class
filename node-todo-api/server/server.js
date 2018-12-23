ca51c759ed5cb68fcb5fc997ad28c1b57ec89419
Jane Kagan <jkagan@cray.com>
2018-12-23 05:29:06 -0600

    Individual resource request

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -24,6 +25,18 @@ app.post('/todos', (req, res) => {
 app.get('/todos', (req, res) => {
 	Todo.find().then((todos) => {
 		res.send({todos});
+	}, (e) => {
+		res.status(400).send(e);
+	});
+});
+
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send(`todo with id ${id} not found`);
+	}
+	Todo.findById(id).then((todo) => {
+		res.send({todo})
 	}, (e) => {
 		res.status(400).send(e);
 	});

7b14ac52abeb63b8ff14354da07688d1a8d50396
Jane Kagan <jkagan@cray.com>
2018-12-23 23:15:35 -0600

    Hash user password before saving

--- node-todo-api/playground/hashing.js
+++ node-todo-api/playground/hashing.js
@@ -1,5 +1,6 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
@@ -27,11 +28,31 @@ const jwt = require('jsonwebtoken');
 // 	console.log('Data was changed, do NOT trust!!!!');
 // }
 
-var data = {
-	id: 10
-};
+// ====
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+// var data = {
+// 	id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
+
+// ====
+
+var password = '123abc!';
+
+// bcrypt.genSalt(10, (err, salt) => {
+// 	bcrypt.hash(password, salt, (err, hash) => {
+// 		console.log(hash);
+// 	});
+// });
+
+var hashedPassword = '$2a$10$fUoQTA9aGpT5OcwzgWI6IeoXBlyTpzXYr6dwPuSkIzfvl0H05FJT.';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+	console.log(res);
+});
+
+
